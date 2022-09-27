1d7a6e1976d3399d35924827c84108e522bac6fd
Aleix <aleix.fdu@gmail.com>
2022-09-27 19:36:21 +0200

    nomes es poden girar dues cartes i form inclou usernames segons nombre que posi

--- program.js
+++ program.js
@@ -1,11 +1,12 @@
-window.addEventListener("load", function(event) {
-  
-    
-  var taula =  document.querySelectorAll(".cela");
+//Per el tauler
+window.addEventListener("load", function (event) {
+
+
+  var taula = document.querySelectorAll(".cela");
 
   for (let i = 0; i < taula.length; i++) {
-  taula[i].addEventListener("click",Mostra);
-  //taula[i].removeEventListener("click");
+    taula[i].addEventListener("click", Mostra);
+    //taula[i].removeEventListener("click");
   }
 
   var torn = 0;
@@ -13,160 +14,103 @@ window.addEventListener("load", function(event) {
   var carta1SRC;
   var carta1Poker;
   var cela1;
-  
+
   var carta2;
   var carta2SRC;
   var carta2Poker;
   var cela2;
 
+ // var llest = true;
 
 
 
- 
+
+  function pausa(c1, c2, c1p, c2p) {
 
 
-  function pausa(c1,c2,c1p,c2p) {
+    setTimeout(amaga, 1000);
 
-    setTimeout(amaga, 2000);
-    
-    function amaga(){
+
+    function amaga() {
+
+      torn = 0
       c1.classList.add("amaga");
       c2.classList.add("amaga");
       c1p.classList.remove("amaga");
       c2p.classList.remove("amaga");
+      //llest = true;
+
     }
   }
 
 
-  
-  
-
-  
-
-  function Mostra(){
-
-      console.log("click");
-      torn++;
-
-      
-       //const music = new Audio('prova.mp3');
-      //music.play();
-      
-     
-      
+  function Mostra() {
 
 
+    console.log("click");
+    torn++;
 
     var cartaImatge = this.querySelector(".parella");
     var cartapoker = this.querySelector(".poker");
-
     //ella mateixa ja que si posem this.querySelector(".poker"); posa
     var cela = this;
-    //cartapoker.classList.add("amaga");
-    //cartaImatge.classList.remove("amaga");
 
 
 
+    switch (torn) {
+      case 1:
+        //llest = false;
+        carta1SRC = cartaImatge.getAttribute('src');
+        carta1 = cartaImatge;
+        carta1Poker = cartapoker;
+        cela1 = cela;
 
 
-if (torn==1) {
-      carta1SRC=cartaImatge.getAttribute('src');
-      carta1=cartaImatge;
-      carta1Poker=cartapoker;
-      cela1=cela;
+        carta1Poker.classList.add("amaga");
+        carta1.classList.remove("amaga");
 
 
-      carta1Poker.classList.add("amaga");
-      carta1.classList.remove("amaga");
-    }
-
-    else{
-      carta2SRC=cartaImatge.getAttribute('src');
-      carta2=cartaImatge;
-      carta2Poker=cartapoker;
-      cela2=cela;
+        break;
 
 
-      //sigui igual o no es mostrarà la imatge
-      carta2Poker.classList.add("amaga");
-      carta2.classList.remove("amaga");
-      
+      case 2:
 
-      if (carta1SRC===carta2SRC) {
-
-      
-
-        if(carta1.getAttribute('id')!=carta2.getAttribute('id')){
-          
-          console.log("es la mateixa");
-            cela1.removeEventListener("click",Mostra);
-            cela2.removeEventListener("click",Mostra);
+        carta2SRC = cartaImatge.getAttribute('src');
+        carta2 = cartaImatge;
+        carta2Poker = cartapoker;
+        cela2 = cela;
 
 
-        }
-        else{
-          console.log("ERROR: has clicat dos cops a una mateixa carta clica'n una altra");
-          // carta1.classList.add("amaga");
-          // carta1Poker.classList.remove("amaga");
+        //sigui igual o no es mostrarà la imatge
+        carta2Poker.classList.add("amaga");
+        carta2.classList.remove("amaga");
 
 
-          //Perquè en comptes de que quan cliquis a sobre i sigui l mateixa casella com he fet abans i tornis de nou a escollir
-          //dues cartes doncs un cop has triat la primera t'obligui a escollir-ne una segona q no sigui la mateixa.
-          //per aixo el torn 3 que detecta que es akest cas i fa lo esmenat.
-          torn=3;
+        if (carta1SRC === carta2SRC) {
 
+
+          if (carta1.getAttribute('id') != carta2.getAttribute('id')) {
+            console.log("es la mateixa");
+            cela1.removeEventListener("click", Mostra);
+            cela2.removeEventListener("click", Mostra);
+            torn = 0;
+          }
+
+          else {
+            //Perque al tornar a clicar el contador augmentarà i serà dos serà 2
+            torn = 1;
+          }
         }
 
 
-        //carta1.classList.remove("amaga");
-        
-        //carta1Poker.classList.add("amaga");
-        //carta2Poker.classList.add("amaga");
-      }
-      else{
+        else {
+          pausa(carta1, carta2, carta1Poker, carta2Poker);
+        }
 
-        pausa(carta1,carta2,carta1Poker,carta2Poker);
-        
-        /**
-         *           carta1.classList.add("amaga");
-        carta2.classList.add("amaga");
-        carta1Poker.classList.remove("amaga");
-        carta2Poker.classList.remove("amaga");
-         */
-
-         
-        
-
-
-      }
-      
-      if(torn!=3){
-        torn=0;
-      }
-      
-
+        break;
     }
- 
-    
-    
-
-
-/**
- *    var style = window.getComputedStyle(cartapoker);
-    var mostra = style.getPropertyValue('display');
-    console.log(mostra);
- */
-
-
-  
 
   }
-
-
-
-
-
-  });
-
-  
-
+});
+//const music = new Audio('prova.mp3');
+//music.play();
---
+++ validateForm.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function(event) {
+    
+    var numNoms = document.querySelector("#players");
+    numNoms.addEventListener("change", comprovaJugadors);
+    var onInserirNoms = document.querySelector("#pJugadors");
+    
+    
+    function comprovaJugadors(){
+
+      onInserirNoms.innerHTML = '';
+
+      // var hola = document.querySelector("#players");
+      // var valor = hola.value;
+      console.log(numNoms.value);
+
+        for (let i = 0; i < numNoms.value; i++) {
+          
+          //console.log(onInserirNoms);
+          onInserirNoms.innerHTML += '<p><input type="text" id="nomJugador'+(i+1)+'" name="nomJugador'+(i+1)+'" placeholder="Nom Jugador '+(i+1)+'" class="nomsTotals"required></p>';
+          
+          
+
+        }
+      }
+});
